32f9c71e116fd1e77ffe3a262e9f9c607bc94492
chenshi <cshi@miuandes.cl>
2024-10-03 16:36:08 -0300

    solo falta modificar la clase region, definiendo el metodo showRegion, para esto se le debe agregar un print al ListOfPoint2D o un print directamente dentro de showRegion

--- imagepro/include/image/region.hpp
+++ imagepro/include/image/region.hpp
@@ -9,7 +9,7 @@ namespace image{
         ListOfPoint2D* region_list;
         public:
         Region();
-        Region(int id, int n_pixeles, ListOfPoint2D lista_region);
+        Region(int id, int n_pixeles, ListOfPoint2D* lista_region);
         void setId(int id);
         void setNumberOfPoints(int n);
         void setRegionList(ListOfPoint2D lista_region);
--- imagepro/src/region.cpp
+++ imagepro/src/region.cpp
@@ -1,3 +1,18 @@
 /* implementation of the class Region
  */
+#include "image/region.hpp"
 
+namespace image{
+
+    Region::Region():id(-1), n_points2d(-1), region_list(nullptr){}
+
+    Region::Region(int id, int n_pixeles, ListOfPoint2D* Lista_region): id(id), n_points2d(n_pixeles), region_list(Lista_region){};
+
+    void Region::showRegion(){
+        
+    }
+
+
+
+
+};
