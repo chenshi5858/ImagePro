b9cfb2c89f39da9dcaa8ac1dbc463ed898730d1a
Chen Shi <cshi@miuandes.cl>
2024-10-11 01:30:04 -0300

    dorime

---
+++ imagepro/build/CMakeFiles/imagepro.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/imagepro.cpp" "CMakeFiles/imagepro.dir/imagepro.cpp.o" "gcc" "CMakeFiles/imagepro.dir/imagepro.cpp.o.d"
+  "/home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/src/image.cpp" "CMakeFiles/imagepro.dir/src/image.cpp.o" "gcc" "CMakeFiles/imagepro.dir/src/image.cpp.o.d"
+  "/home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/src/listofpoint2d.cpp" "CMakeFiles/imagepro.dir/src/listofpoint2d.cpp.o" "gcc" "CMakeFiles/imagepro.dir/src/listofpoint2d.cpp.o.d"
+  "/home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/src/listofregion.cpp" "CMakeFiles/imagepro.dir/src/listofregion.cpp.o" "gcc" "CMakeFiles/imagepro.dir/src/listofregion.cpp.o.d"
+  "/home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/src/nodepoint2d.cpp" "CMakeFiles/imagepro.dir/src/nodepoint2d.cpp.o" "gcc" "CMakeFiles/imagepro.dir/src/nodepoint2d.cpp.o.d"
+  "/home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/src/noderegion.cpp" "CMakeFiles/imagepro.dir/src/noderegion.cpp.o" "gcc" "CMakeFiles/imagepro.dir/src/noderegion.cpp.o.d"
+  "/home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/src/point2D.cpp" "CMakeFiles/imagepro.dir/src/point2D.cpp.o" "gcc" "CMakeFiles/imagepro.dir/src/point2D.cpp.o.d"
+  "/home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/src/region.cpp" "CMakeFiles/imagepro.dir/src/region.cpp.o" "gcc" "CMakeFiles/imagepro.dir/src/region.cpp.o.d"
+  "/home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/src/stackofnodepoint2d.cpp" "CMakeFiles/imagepro.dir/src/stackofnodepoint2d.cpp.o" "gcc" "CMakeFiles/imagepro.dir/src/stackofnodepoint2d.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
