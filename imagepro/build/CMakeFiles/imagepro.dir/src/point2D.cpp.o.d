83bb8151bb4b75a4ab40d1e1675cd49ca747e679
Chen Shi <cshi@miuandes.cl>
2024-10-12 04:16:31 -0300

    terminado piquiin

---
+++ imagepro/build/CMakeFiles/imagepro.dir/src/point2D.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/imagepro.dir/src/point2D.cpp.o: \
+ /home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/src/point2D.cpp \
+ /usr/include/stdc-predef.h \
+ /home/edauandes/Desktop/tarea2eda/ImagePro/imagepro/include/image/point2D.hpp
